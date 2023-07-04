e0b3f195186ad29c81b761ada37756d655efac7c
Marcoslacroix <marcos.lacroix@tdsoft.com.br>
2023-07-04 19:16:42 -0300

    adicionado evento e empresa no modelo do lots

--- models/lots.js
+++ models/lots.js
@@ -1,5 +1,7 @@
 const { DataTypes } = require("sequelize");
 const { sequelize } = require('../config/database');
+const Event = require("../models/event");
+const Company = require("../models/company");
 
 const Lots = sequelize.define('Lots', {
     id: {
@@ -19,12 +21,29 @@ const Lots = sequelize.define('Lots', {
       type: DataTypes.DATE,
       allowNull: false,
     },
+    active: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false
+    },
+    event_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    }
   }, 
   {
       freezeTableName: true,
       timestamps: false,
       tableName: 'lots'
-  }
-  );
+  });
+
+  Lots.belongsTo(Event, {
+    foreignKey: 'event_id',
+    onDelete: 'CASCADE',
+  });
+
+  Lots.belongsTo(Company, {
+    foreignKey: 'company_id',
+    onDelete: 'CASCADE',
+  });
   
   module.exports = Lots;
